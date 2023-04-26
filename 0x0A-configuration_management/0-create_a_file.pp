42d6d4bf0002bd7e78cb75c0cc4560e0ab8d1396
Comfort Ajayi <ajayicomfort08@gmail.com>
2023-04-26 09:47:09 -0700

    puppet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # creates a file
 file { '/tmp/school':
 	mode	 => '0744',
@@ -6,14 +5,4 @@ file { '/tmp/school':
 	group	 => 'www.data',
 	content  => 'I love Puppet'
 }
-	
-=======
-# creates a file in /tmp
 
-file { '/tmp/school':
-  content =>'I love Puppet',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-}
->>>>>>> 1aab3fc7b971c45911a5fdd74993c9040c9e5ef7
